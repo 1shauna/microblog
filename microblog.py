bca6aa35de565fd7f3854e37e511833ebe90b9a0
1shauna <shauna.kerr@gmail.com>
2020-03-11 20:22:08 -0400

    Add database, shell context

--- microblog.py
+++ microblog.py
@@ -1,2 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
+# create a shell conntext
+# add the db instance and models (User, Post) to the shell session
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
