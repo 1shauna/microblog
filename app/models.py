d4dbf18091586595f84d4239f7b53eabe5dcf452
1shauna <shauna.kerr@gmail.com>
2020-03-15 23:04:45 -0400

    Followers, Tests

--- app/models.py
+++ app/models.py
@@ -4,6 +4,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+# Since this is an auxiliary table that has no data other than
+# the foreign keys, it's created without an associated model class.
+followers = db.Table('followers',
+	db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
 	""" fields:
 	id INTEGER primary key
@@ -44,6 +52,54 @@ class User(UserMixin, db.Model):
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
+	# `followed` is from the point of view of the left side of relationship tables
+	followed = db.relationship(
+		'User', # right side of relationship (users)
+		secondary=followers, # left side of relationship (followers)
+		primaryjoin=(followers.c.follower_id == id), # condition that links the left with the association table
+		secondaryjoin=(followers.c.followed_id == id), # condition that links the right with the association table
+		backref=db.backref('followers', lazy='dynamic'), # how this relationship will be accessed from the right side (from the left, the relationship is named `followed`)
+		lazy='dynamic')
+	# =====================================================================
+	# `db.relationship` function defines the relationship in the model class.
+	# This relationship links User instances to other User instances,
+	# so as a convention let's say that for a pair of users linked by
+	# this relationship, the left side user is following the right side user.
+	# I'm defining the relationship as seen from the left side user with
+	# the name followed, because when I query this relationship from
+	# the left side I will get the list of followed users
+	# (i.e those on the right side).
+	#
+	# 'User' is the right side entity of the relationship
+	# (the left side entity is the parent class). Since this is
+	# a self-referential relationship, I have to use the same class
+	# on both sides.
+	# Secondary configures the association table that is used
+	# for this relationship, which I defined right above this class.
+	# primaryjoin indicates the condition that links the left side entity
+	# (the follower user) with the association table.
+	# The join condition for the left side of the relationship is
+	# the user ID matching the follower_id field of the association table.
+	# The followers.c.follower_id expression references the follower_id
+	# column of the association table.
+	# secondaryjoin indicates the condition that links the right side entity
+	# (the followed user) with the association table. This condition
+	# is similar to the one for primaryjoin, with the only difference
+	# that now I'm using followed_id, which is the other foreign key in
+	# the association table.
+	# backref defines how this relationship will be accessed from
+	# the right side entity. From the left side, the relationship
+	# is named followed, so from the right side I am going to use
+	# the name followers to represent all the left side users that
+	# are linked to the target user in the right side. The additional
+	# lazy argument indicates the execution mode for this query.
+	# A mode of dynamic sets up the query to not run until specifically
+	# requested, which is also how I set up the posts one-to-many relationship.
+	# Lazy is similar to the parameter of the same name in the backref,
+	# but this one applies to the left side query instead of the right side.
+	# =====================================================================
+
+
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
 	# ===========================================================
@@ -75,6 +131,36 @@ class User(UserMixin, db.Model):
 	# the avatar() method to return different URLs, and all the templates
 	# will start showing the new avatars automatically.
 	# ================================================================
+
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(
+			followers.c.followed_id == user.id
+			).count() > 0
+
+	# in order to display recent posts by authors that current_user follows:
+	def followed_posts(self):
+		followed = Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+				followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
+		# `followed` is queried on the Posts table: join the followers table
+		# on followers.followed_id == Post.user_id.
+		# Then filter where the follower's id is the current_user's id.
+		# `own` is the user's own posts, because apparently users (evwhere)
+		# like to see their own posts on their followed posts. Dunno.
+		# Then join these 2 queries with union,
+		# and order by the post's timestamp, descending (most recent first)
+
+
 
 # command line: flask db init
 # ==============================================================
@@ -133,3 +219,4 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
+
